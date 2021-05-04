8095b5f3a35f3ccf487335b721fcbce006c8c31f
xeroxzen <andile.mbele@firstsourcetech.africa>
2021-05-04 10:58:36 +0200

    PersistantDrawerLeft

--- extracity-admin-dashboard/src/components/reservations/ReservationAdd.jsx
+++ extracity-admin-dashboard/src/components/reservations/ReservationAdd.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ReservationAdd() {
     return (
         <div>
-            <p>We'll add something here</p>
+            <p>We should have something here up soon</p>
         </div>
     )
 }
