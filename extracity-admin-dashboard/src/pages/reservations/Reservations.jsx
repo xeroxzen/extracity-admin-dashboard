9a117c979a56799eee66fc53da0b669a37b2aabf
THamsanqa <thamsanqampofu.tcmc@gmail.com>
2021-05-06 09:42:45 +0200

    Reservations

---
+++ extracity-admin-dashboard/src/pages/reservations/Reservations.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../../components/layout/Footer'
+// import Navbar from '../../components/layout/Navbar'
+import Sidebar from '../../components/layout/Sidebar'
+// import ReservationTable from '../../components/reservations/ReservationTable'
+import DataGrid from '../../components/reservations/DataGrid'
+
+export default function Reservations() {
+    return (
+        <div>
+            <Sidebar />
+            {/* <ReservationTable /> */}
+            <DataGrid />
+            <Footer />
+        </div>
+    )
+}
