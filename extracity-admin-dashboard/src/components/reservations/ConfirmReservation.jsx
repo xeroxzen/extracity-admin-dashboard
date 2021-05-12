a3adb7248f561d4b276a84fd239e4a544d0810c1
xeroxzen <andilembele020@gmail.com>
2021-05-12 14:18:28 +0200

    refactored #12

--- extracity-admin-dashboard/src/components/reservations/ConfirmReservation.jsx
+++ extracity-admin-dashboard/src/components/reservations/ConfirmReservation.jsx
@@ -8,34 +8,34 @@ import Grid from '@material-ui/core/Grid';
 var moment = require("moment");
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        flexGrow: 1,
-    },
-    margin: {
-        margin: theme.spacing(1),
-    },
-    withoutLabel: {
-        marginTop: theme.spacing(3),
-    },
-    table: {
-      width: '100%',
-    },
-    pricesContainer: {
-      padding: theme.spacing(1),
-    },
-    paper: {
-      height: 140,
-      width: 100,
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-    fullWidth: {
-      width: '100%',
-      margin: theme.spacing(0.5),
-    },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexGrow: 1,
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  table: {
+    width: '100%',
+  },
+  pricesContainer: {
+    padding: theme.spacing(1),
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+  fullWidth: {
+    width: '100%',
+    margin: theme.spacing(0.5),
+  },
 }));
 
 
@@ -44,27 +44,27 @@ export default function SearchTripForm(props) {
 
   let submitButton = null;
 
-  if (props.values.trip!==null && props.values.time!==null){
-      submitButton = 
-    (<Modal.Footer>
-      <p><b>Note:</b> Please click the save button after you have confirmed the information displayed above.</p>
+  if (props.values.trip !== null && props.values.time !== null) {
+    submitButton =
+      (<Modal.Footer>
+        <p><b>Note:</b> Please click the save button after you have confirmed the information displayed above.</p>
         <Button
-            onClick={props.handleSubmit}
-            variant="contained"
-            color="primary"
-            className={classes.button}>
-              Save
+          onClick={props.handleSubmit}
+          variant="contained"
+          color="primary"
+          className={classes.button}>
+          Save
         </Button>
-    </Modal.Footer>);
+      </Modal.Footer>);
   }
-  else{
-    
+  else {
+
   }
 
   let formatter = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: props.reservation.Currency,
-      });
+    style: "currency",
+    currency: props.reservation.Currency,
+  });
   return (
     <Modal
       {...props}
@@ -79,93 +79,93 @@ export default function SearchTripForm(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-         <div className={classes.root}>
-            <Grid container className={classes.root} spacing={2}>
-              <Grid item lg={6}>
-                <Grid container justify="left" spacing={1}>
-                  <h3>Ticket information</h3>
-                  <table className={classes.fullWidth}>
-                    <tr>
-                      <td><b>Route:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.reservation.Trip}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Travelling from:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.reservation.TravellingFrom}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Travelling to:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.reservation.TravellingTo}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Date:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{moment(props.reservation.Date.toDate()).format('LL')}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Time (Leaves Bulawayo at):</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.reservation.TravelTime}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Amount:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.reservation.Currency}{formatter.format(props.reservation.Amount).replace(props.reservation.Currency,"").replace("$","")}</td>
-                    </tr>
-                  </table>
-                </Grid>
-              </Grid>
-              <Grid item xs={6}>
-                <Grid container justify="left" spacing={1}>
-                  <h3>Personal information</h3>
-                  <table className={classes.fullWidth}>
-                    <tr>
-                      <td><b>First name:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.values.firstname}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Last name:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.values.lastname}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Email:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.values.email}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Phone number:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.values.phoneNumber}</td>
-                    </tr>
-                    <tr>
-                      <td><b>Paid using:</b></td>
-                    </tr>
-                    <tr>
-                      <td>{props.values.paymentMethod}</td>
-                    </tr>
-                  </table>
-                </Grid>
+        <div className={classes.root}>
+          <Grid container className={classes.root} spacing={2}>
+            <Grid item lg={6}>
+              <Grid container justify="left" spacing={1}>
+                <h3>Ticket information</h3>
+                <table className={classes.fullWidth}>
+                  <tr>
+                    <td><b>Route:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.reservation.Trip}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Travelling from:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.reservation.TravellingFrom}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Travelling to:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.reservation.TravellingTo}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Date:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{moment(props.reservation.Date.toDate()).format('LL')}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Time (Leaves Bulawayo at):</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.reservation.TravelTime}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Amount:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.reservation.Currency}{formatter.format(props.reservation.Amount).replace(props.reservation.Currency, "").replace("$", "")}</td>
+                  </tr>
+                </table>
               </Grid>
             </Grid>
+            <Grid item xs={6}>
+              <Grid container justify="left" spacing={1}>
+                <h3>Personal information</h3>
+                <table className={classes.fullWidth}>
+                  <tr>
+                    <td><b>First name:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.values.firstname}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Last name:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.values.lastname}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Email:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.values.email}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Phone number:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.values.phoneNumber}</td>
+                  </tr>
+                  <tr>
+                    <td><b>Paid using:</b></td>
+                  </tr>
+                  <tr>
+                    <td>{props.values.paymentMethod}</td>
+                  </tr>
+                </table>
+              </Grid>
+            </Grid>
+          </Grid>
         </div>
       </Modal.Body>
       {
-       submitButton
+        submitButton
       }
     </Modal>
   );
