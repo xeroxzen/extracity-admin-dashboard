cc39774d7cf2c490c48033a84b51328cfd759125
xeroxzen <andilembele020@gmail.com>
2021-05-14 10:16:24 +0200

    last refactored code before 1st production version

--- extracity-admin-dashboard/src/components/reservations/DataGrid.jsx
+++ extracity-admin-dashboard/src/components/reservations/DataGrid.jsx
@@ -3,9 +3,6 @@ import clsx from 'clsx';
 import { DataGrid } from '@material-ui/data-grid';
 import firebase from "../../firebase.config";
 import { makeStyles } from "@material-ui/core/styles";
-
-// import moment from 'moment'
-import { Button } from '@material-ui/core';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Grid from '@material-ui/core/Grid';
@@ -24,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: '10px',
     },
     filterBox: {
-         margin: theme.spacing(1),
+        margin: theme.spacing(1),
     }
 }));
 
@@ -42,20 +39,9 @@ const headCells = [
     { field: "Trip", headerName: "TRIP", width: 170 },
     {
         field: "Date", headerName: "DATE", width: 160, valueGetter: params => `${params.row.Date.toDate()}`,
-    }, //moment().startOf('day').fromNow(); 
+    },
     { field: "TravelTime", headerName: "TIME", width: 110 },
-    // { field: "Email", headerName: "EMAIL", width: 130 },
     { field: "Amount", headerName: "AMOUNT", width: 110, valueGetter: params => `${params.row.Currency} ${params.row.Amount}` },
-    // {
-    //     field: "PaymentMethod",
-    //     headerName: "PAYMENT METHOD",
-    //     width: 130
-    // },
-    // {
-    //     field: "MobileMoneyAccount",
-    //     headerName: "PAYMENT ACCOUNT",
-    //     width: 130
-    // },
     { field: "TicketID", headerName: "TICKET ID", width: 180 },
 ]
 
@@ -73,9 +59,9 @@ export default function DataTable() {
     });
     const db = firebase.firestore();
 
-    const handleChange = (prop,value) => {
+    const handleChange = (prop, value) => {
         setValues({ ...values, [prop]: value });
-        fetchReservations(null,prop,value);
+        fetchReservations(null, prop, value);
     }
 
     const handleTo = (value) => {
@@ -98,7 +84,7 @@ export default function DataTable() {
 
     const fetchReservations = async (r = null, property = null, value = null) => {
         let rts;
-        if (r===null)rts = routes;
+        if (r === null) rts = routes;
         else rts = r;
 
 
@@ -107,9 +93,10 @@ export default function DataTable() {
         let route = values.route;
         let trip = values.trip;
         let time = values.time;
+        // eslint-disable-next-line
         let search = values.search;
 
-        switch(property){
+        switch (property) {
             case 'to':
                 to = value;
                 break;
@@ -124,37 +111,42 @@ export default function DataTable() {
                 break;
             case 'time':
                 time = value;
+                break;
             case 'search':
+                // eslint-disable-next-line
                 search = value;
+                break;
+            default:
+                console.log('No data')
         }
 
         const data = db.collection('reservations');
         let queryRef = data.where('status', '==', 'paid');
 
-        if (route !== '' && rts[route] !== undefined){
-            queryRef = queryRef.where("Trip","==",rts[route]?.name);
-            if (trip !== '' && rts[route].possibleTrips[trip] !== undefined){
+        if (route !== '' && rts[route] !== undefined) {
+            queryRef = queryRef.where("Trip", "==", rts[route]?.name);
+            if (trip !== '' && rts[route].possibleTrips[trip] !== undefined) {
                 let arr = rts[route].possibleTrips[trip].split('-');
                 queryRef = queryRef
-                                .where("TravellingFrom","==",arr[0])
-                                .where("TravellingTo","==",arr[1]);
+                    .where("TravellingFrom", "==", arr[0])
+                    .where("TravellingTo", "==", arr[1]);
             }
 
-            if (time !== '' && rts[route].times[time] !== undefined)queryRef = queryRef.where("TravelTime","==",rts[route]?.times[time]);
+            if (time !== '' && rts[route].times[time] !== undefined) queryRef = queryRef.where("TravelTime", "==", rts[route]?.times[time]);
         }
-        console.log(from,to);
-        if (from !== '' && from !== null && from !== undefined && from === to){
+        console.log(from, to);
+        if (from !== '' && from !== null && from !== undefined && from === to) {
             console.log('heer');
             let min = moment(from, "YYYY-MM-DD").toDate();
             min.setDays(min.getDays() - 1);
             let max = moment(from, "YYYY-MM-DD").toDate();
             max.setDays(max.getDays() + 1);
 
-            queryRef = queryRef.where("Date", ">", min).where("Date","<",max);
+            queryRef = queryRef.where("Date", ">", min).where("Date", "<", max);
         }
-        else{
-            if (from !== '' && from !== null && from !== undefined)queryRef = queryRef.where("Date", ">=", moment(from, "YYYY-MM-DD").toDate());
-            if (to !== '' && to !== null && to !== undefined)queryRef = queryRef.where("Date", "<=", moment(to, "YYYY-MM-DD").toDate());
+        else {
+            if (from !== '' && from !== null && from !== undefined) queryRef = queryRef.where("Date", ">=", moment(from, "YYYY-MM-DD").toDate());
+            if (to !== '' && to !== null && to !== undefined) queryRef = queryRef.where("Date", "<=", moment(to, "YYYY-MM-DD").toDate());
         }
         const query = await queryRef.get();
         console.log(query.size);
@@ -169,107 +161,108 @@ export default function DataTable() {
 
     return (
         <div className={classes.root}>
-            <h3 style={{ marginBottom: 20, }}>Ticket Reservation List</h3> <Button
+            <h3 style={{ marginBottom: 20, }}>Ticket Reservation List</h3>
+            {/* <Button
                 variant="contained"
                 color="primary"
                 href="/reservations/add-reservation"
-                className={classes.button}>Add Reservation</Button>
+                className={classes.button}>Add Reservation</Button> */}
             <br />
-             <Grid container className={classes.filterBox} spacing={2}>
+            <Grid container className={classes.filterBox} spacing={2}>
                 <Grid item>
-                  <Grid container justify="left" spacing={1}>
-                    <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
-                      <select
-                        labelId="id"
-                        value={values.route}
-                        onChange={(e) => handleChange('route', e.target.value)}
-                        required
-                      >
-                        <option value="">All</option>
-                        {routes?.map((item, counter) => {
-                          return <option value={counter}>{item.name}</option>;
-                        })}
-                      </select>
-                      <FormHelperText id="standard-date-helper-text">Route</FormHelperText>
-                    </FormControl>
-                   </Grid>
-                </Grid>
-                <Grid item>
-                  <Grid container justify="left" spacing={1}>
-                    <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
-                      <select
-                        labelId="id"
-                        value={values.trip}
-                        onChange={(e) => handleChange('trip', e.target.value)}
-                        required
-                      >
-                        <option value="">All</option>
-                        {routes[values.route]?.possibleTrips?.map((item, key) => {
-                          return <option value={key}>{item.replace('-', " to ")}</option>;
-                        })}
-                      </select>
-                      <FormHelperText id="standard-date-helper-text">Trip</FormHelperText>
-                    </FormControl>
-                   </Grid>
-                </Grid>
-                <Grid item>
-                  <Grid container justify="left" spacing={1}>
-                    <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
-                      <select
-                        labelId="id"
-                        value={values.time}
-                        onChange={(e) => handleChange('time', e.target.value)}
-                        required
-                      >
-                        <option value="">All</option>
-                        {routes[values.route]?.times?.map((item, counter) => {
-                          return <option value={counter}>{item}</option>;
-                        })}
-                      </select>
-                      <FormHelperText id="standard-date-helper-text">Time</FormHelperText>
-                    </FormControl>
-                   </Grid>
-                </Grid>
-                <Grid item>
-                   <Grid container justify="left" spacing={1}>
+                    <Grid container justify="left" spacing={1}>
                         <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
-                          <DatePicker
-                            onChange={handleFrom}
-                            value={values.from}
-                          />
-                          <FormHelperText id="standard-date-helper-text">From</FormHelperText>
+                            <select
+                                labelId="id"
+                                value={values.route}
+                                onChange={(e) => handleChange('route', e.target.value)}
+                                required
+                            >
+                                <option value="">All</option>
+                                {routes?.map((item, counter) => {
+                                    return <option value={counter}>{item.name}</option>;
+                                })}
+                            </select>
+                            <FormHelperText id="standard-date-helper-text">Route</FormHelperText>
                         </FormControl>
-                   </Grid>
+                    </Grid>
                 </Grid>
                 <Grid item>
-                   <Grid container justify="left" spacing={1}>
+                    <Grid container justify="left" spacing={1}>
                         <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
-                          <DatePicker
-                            onChange={handleTo}
-                            value={values.to}
-                          />
-                          <FormHelperText id="standard-date-helper-text">To</FormHelperText>
+                            <select
+                                labelId="id"
+                                value={values.trip}
+                                onChange={(e) => handleChange('trip', e.target.value)}
+                                required
+                            >
+                                <option value="">All</option>
+                                {routes[values.route]?.possibleTrips?.map((item, key) => {
+                                    return <option value={key}>{item.replace('-', " to ")}</option>;
+                                })}
+                            </select>
+                            <FormHelperText id="standard-date-helper-text">Trip</FormHelperText>
                         </FormControl>
-                   </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container justify="left" spacing={1}>
+                        <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
+                            <select
+                                labelId="id"
+                                value={values.time}
+                                onChange={(e) => handleChange('time', e.target.value)}
+                                required
+                            >
+                                <option value="">All</option>
+                                {routes[values.route]?.times?.map((item, counter) => {
+                                    return <option value={counter}>{item}</option>;
+                                })}
+                            </select>
+                            <FormHelperText id="standard-date-helper-text">Time</FormHelperText>
+                        </FormControl>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container justify="left" spacing={1}>
+                        <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
+                            <DatePicker
+                                onChange={handleFrom}
+                                value={values.from}
+                            />
+                            <FormHelperText id="standard-date-helper-text">From</FormHelperText>
+                        </FormControl>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container justify="left" spacing={1}>
+                        <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
+                            <DatePicker
+                                onChange={handleTo}
+                                value={values.to}
+                            />
+                            <FormHelperText id="standard-date-helper-text">To</FormHelperText>
+                        </FormControl>
+                    </Grid>
                 </Grid>
                 <Grid item>
                     <Grid container justify="left" spacing={1}>
                         <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
                             <Input
-                              id="standard-adornment-search"
-                              placeholder="Search..."
-                              value={values.search}
-                              required
-                              onChange={(e) => handleChange('search', e.target.value)}
-                              endAdornment={<InputAdornment position="end"></InputAdornment>}
-                              aria-describedby="standard-payment-reference-helper-text"
-                              inputProps={{
-                                'aria-label': 'Search',
-                              }}
+                                id="standard-adornment-search"
+                                placeholder="Search..."
+                                value={values.search}
+                                required
+                                onChange={(e) => handleChange('search', e.target.value)}
+                                endAdornment={<InputAdornment position="end"></InputAdornment>}
+                                aria-describedby="standard-payment-reference-helper-text"
+                                inputProps={{
+                                    'aria-label': 'Search',
+                                }}
                             >
                             </Input>
                             <FormHelperText id="standard-search-helper-text">Search</FormHelperText>
-                          </FormControl>
+                        </FormControl>
                     </Grid>
                 </Grid>
             </Grid>
