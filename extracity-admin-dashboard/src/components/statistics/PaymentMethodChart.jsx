7b73f19da5cf5c45ea7d33da40d25c926a746f50
xeroxzen <andilembele020@gmail.com>
2021-05-21 12:21:16 +0200

    PaymentMethodChart

---
+++ extracity-admin-dashboard/src/components/statistics/PaymentMethodChart.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const data = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+    datasets: [
+        {
+            label: 'EcoCash',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgb(255, 99, 132)',
+        },
+        {
+            label: 'Cash',
+            data: [2, 3, 20, 5, 1, 4],
+            backgroundColor: 'rgb(54, 162, 235)',
+        },
+        {
+            label: 'One Money',
+            data: [3, 10, 13, 15, 22, 30],
+            backgroundColor: 'rgb(75, 192, 192)',
+        },
+    ],
+};
+
+const options = {
+    scales: {
+        yAxes: [
+            {
+                stacked: true,
+                ticks: {
+                    beginAtZero: true,
+                },
+            },
+        ],
+        xAxes: [
+            {
+                stacked: true,
+            },
+        ],
+    },
+};
+
+const PaymentMethodChart = () => (
+    <>
+        <div className='header'>
+            <h5 style={{ color: '#fff' }}>Payment Methods Chart</h5>
+            <div className='links'>
+                <a
+                    className='btn btn-gh'
+                    href='https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/StackedBar.js'
+                >
+
+                </a>
+            </div>
+        </div>
+        <Bar data={data} options={options} />
+    </>
+);
+
+export default PaymentMethodChart;
